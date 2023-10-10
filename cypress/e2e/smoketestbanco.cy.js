a8f79eec7b6a84479dd15ba335bc8c522536c408
Andrea Barrera <andrea.barrera7@gmail.com>
2023-10-09 19:31:23 -0500

    Creating second part in firs test

--- cypress/e2e/smoketestbanco.cy.js
+++ cypress/e2e/smoketestbanco.cy.js
@@ -1,5 +1,7 @@
 describe("Test suite - conjunto de pruebas", () => {
   it("Validar pagina de inicio", () => {
     cy.visit("zero.webappsecurity.com");
+    cy.get(".active > img").should("be.visible");
+    cy.get(".active > .custom > h4").contains("Online Banking");
   });
 });
